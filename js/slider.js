11525fad7ce74b52c7a194f716c7132036784c4f
natalie gomez <nrg290@nyu.edu>
2019-05-09 17:00:47 -0400

    First try where I try to copy chris' example from class (but just the html, css and js)

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+var stepSlider = document.getElementById('slider-step');
+
+noUiSlider.create(stepSlider, {
+    start: [4000],
+    step: 1000,
+    range: {
+        'min': [2000],
+        'max': [10000]
+    }
+});
+
+var stepSliderValueElement = document.getElementById('slider-step-value');
+
+stepSlider.noUiSlider.on('update', function (values, handle) {
+    const value = values[handle];
+    console.log(value)
+    stepSliderValueElement.innerHTML = values[handle];
+});
